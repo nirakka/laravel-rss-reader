39bfd999abde8750fcc71e361af95445c297eba4
vagrant <vagrant@homestead>
2016-09-20 05:48:58 +0000

    editted HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\VipperOre;
+use App\memo;
 
 class HomeController extends Controller
 {
@@ -24,30 +25,87 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $articles = VipperOre::all();
+        $articles = VipperOre::orderBy('date', 'desc')->get();
         return view('home', ['articles' => $articles]);
     }
 
     public function store()
     {
-        $xml = simplexml_load_file('http://blog.livedoor.jp/news23vip/atom.xml');
-        $articles = $xml->entry;
-        $site_title = $xml->title;
-        $site_url = $xml->link['href'];
-        foreach ($articles as $item)
-        {
-            $query = new VipperOre();
-            $query->title = $item->title;
-            $query->content = $item->summary;
-            $query->date = $item->issued;
-            $query->url = $item->link['href'];
-            $query->site_title = $site_title;
-            $query->site_url = $site_url;
+        $xmls = [
+            'http://blog.livedoor.jp/news23vip/atom.xml',
+            'http://nirakka.net/blog/?feed=rss2',
+        ];
 
-            $query->save();
+        foreach ($xmls as $xml){
+            $xml = simplexml_load_file($xml);
+            //$xml = simplexml_load_file('http://nirakka.net/blog/?feed=rss2');
+            if ($xml->entry){
+                $articles = $xml->entry;
+                $site_title = $xml->title;
+                $site_url = $xml->link['href'];
+                foreach ($articles as $item)
+                {
+                    $query = new VipperOre();
+                    $query->title = $item->title;
+                    $query->content = $item->summary;
+                    $query->date = $item->issued;
+                    $query->url = $item->link['href'];
+                    $query->site_title = $site_title;
+                    $query->site_url = $site_url;
+
+                    $sub = VipperOre::where('url', '=', $item->url);
+
+                    if (is_null($sub)){
+                        $query->save();
+                    }
+                }
+            } else if ($xml->item){
+                $this->storeRss1($xml);
+            } else {
+                $this->storeRss2($xml);
+            }
         }
 
         return "success!";
     }
+
+    public function storeRss1($xml){
+        
+        
+        $site_title = $xml->channel->title; // サイトのタイトル
+		$site_link = $xml->channel->link; // サイトのリンク
+		foreach($xml->item as $item){
+            $query = new Model();
+            $nameSpace = $xml->getNamespaces(true);
+            $gNode = $item->children($nameSpace['dc']);
+            $query->title = $item->title;
+            $query->url = $item->link;
+            $query->content = $gNode->subject;
+            $query->date = $gNode->date;
+
+            $query->save();
+        }
+    }
+
+    public function storeRss2($xml){
+        $site_title = $xml->channel->title;
+		$site_link	= $xml->channel->link;
+		foreach($xml->channel->item as $item){
+            $query = new memo();
+			$query->title	= $item->title;
+			$query->url	= $item->link;
+			$date	= $item->pubDate;
+			$query->date	= date("Y-m-d H:i:s",strtotime($date));
+            $query->content = $item->description;
+            $query->site_url = $site_link;
+            $query->site_title = $site_title;
+            $sub = VipperOre::where('url', '=', $item->url);
+            if (is_null($sub)){
+            $query->save();
+            }
+        }
+    }
         
 }
+
+
---
+++ database/migrations/2016_09_20_111400_memo.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Memo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('memo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('content');
+            $table->string('date');
+            $table->string('url')->unique();
+            $table->string('site_url');
+            $table->string('site_title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('memo');
+    }
+}
