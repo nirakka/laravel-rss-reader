283fb1620a40e623ccd83216d8d6014445c48867
nirakka <ikoa.syo@gmail.com>
2016-10-17 11:37:35 +0900

    Editted home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,14 +17,16 @@
                                     &nbsp;
                                     <a href={{ $i->url }} target="_blank">{{ $i->title  }}</a>&nbsp;
                                     <a href="{{ $i->site()->first()->site_url }}" target="_blank">{{ $i->site()->first()->site_title }}</a>
-                                    <form method="post" action="{{ url('/follow') }}">
-                                        {{ csrf_field() }}
-	                                <div id="article_stock">
+	                            <div id="{{ $i->id }}" class="clearfix">
+                                        <form method="post" action="{{ url('/follow') }}">
+                                            {{ csrf_field() }}
+
                                             <input type="hidden" name="user_id" value="{{ \Auth::user()->id }}" id="user_id" />
 		                            <input type="hidden" name="article_id" value="{{ $i->id }}" id="article_id"/>
-		                            <button type="submit" alt="ストック" class="crud-submit" id="send"/>
-	                                </div>
-                                    </form>
+		                            <button type="submit" alt="ストック" class="crud-submit btn btn-primary pull-right" data-id="{{ $i->id }}">ストック</button>
+
+                                        </form>
+	                            </div>
                                 </li>
                             @endforeach
                         </ul>
@@ -41,21 +43,27 @@
     @parent
     <script type="text/javascript">
 
+     
      $(function() {
-	 
+	 $.ajaxSetup({
+             headers: {
+                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+             }
+         });
 	 /* Create new Item */
          $(".crud-submit").click(function(e){
              e.preventDefault();
-             var form_action = $("#article_stock").find("form").attr("action");
-             var article_id = $("#article_stock").find("input[name='article_id']").val();
-             var user_id = $("#article_stock").find("input[name='user_id']").val();
+             var article_id = $(this).data('id');
+             var form_action = $("#" + article_id).find("form").attr("action");
+
+             var user_id = $("#" + article_id).find("input[name='user_id']").val();
              $.ajax({
                  dataType: 'json',
                  type:'POST',
                  url: form_action,
                  data:{user_id:user_id,article_id:article_id}
              }).done(function(data){
-                 alert(data);
+
 
              });
 
