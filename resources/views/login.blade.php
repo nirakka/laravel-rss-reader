e7722cfa75ec8de49fb2275f98e934676e33de84
NobuyukiOISHI <b.b.bluescratch@gmail.com>
2016-10-23 02:30:29 +0900

    registerをつけました

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 
 	            <div id="wrap">
-                <div id="signin-box">
-                    <div id="signin-top">
-                        <p id="signin-title">Sign in</p>
-                    </div>
-                    <div id="signin-main">
+	                <div id="signin-box">
+	                    <div id="signin-top">
+	                        <p id="signin-title">Sign in</p>
+	                    </div>
+	                    <div id="signin-main">
 
-                        <p class="email-inForm-text">E-Mail Address</br>
-                        <input type="text" name="mail" size="30" maxlength="30"></p>
-                        <p class="pass-inForm-text">Password</br>
-                        <input type="password" name="password" size="30" maxlength="20"></p>
-                        <label><p class="rememberMe"><input type="checkbox" name="RememberFg" value="1"> Remember Me</p></label>
-                        <p><button type="button" name="signin">Sign in</button> <a href="#"> Forgot Your Password?</a></p>
-                    </div>
-                </div>
-            </div>
+	                        <p class="email-inForm-text">E-Mail Address</br>
+	                        <input type="text" name="mail" size="30" maxlength="30"></p>
+	                        <p class="pass-inForm-text">Password</br>
+	                        <input type="password" name="password" size="30" maxlength="20"></p>
+	                        <label><p class="rememberMe"><input type="checkbox" name="RememberFg" value="1"> Remember Me</p></label>
+	                        <p><button type="button" name="signin">Sign in</button> <a href="#"> Forgot Your Password?</a></p>
+	                    </div>
+	                </div>
+            	</div>
